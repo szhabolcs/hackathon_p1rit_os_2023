416184036c5c329335d237310c123ca666ae28ce
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-03-18 20:06:25 +0200

    working on generelizing lidl scraper

--- backend/src/services/ScrapeService.ts
+++ backend/src/services/ScrapeService.ts
@@ -3,6 +3,10 @@ import { PrismaClient } from "@prisma/client";
 import env from "dotenv";
 import process from "process";
 const prisma = new PrismaClient();
+const browser = await puppeteer.launch({
+  executablePath: process.env.PUPPETEER_BROWSER_PATH,
+});
+
 // Todo:
 /***
  - scrape carrefour website
@@ -19,50 +23,79 @@ async function kaufland() {}
 
 async function lidl() {
   // Launch browser
-  const browser = await puppeteer.launch({
-    executablePath: process.env.PUPPETEER_BROWSER_PATH,
-  });
 
   // Open page
   const page = await browser.newPage();
+  await page.goto("https://lidl.ro");
 
+  await page.click(".cookie-alert-extended-button");
   // document.querySelectorAll("a.theme__item").forEach((link) => console.log("https://lidl.ro" + link.getAttribute("href")))
-  
+
   try {
-    doOneCategory(page, "");
-    
+    // doOneCategory(page, "");
+    const links = await page.evaluate(() => {
+      const links = document.querySelectorAll("a.theme__item");
+      return Array.from(links).map((link) => {
+        // Get link
+        const page = link.getAttribute("href");
+        const baseUri = "https://lidl.ro" + page;
+        return baseUri;
+      });
+    });
+
+    links.forEach(async (link) => {
+      try {
+        await doOneCategory(link);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    console.log(links);
   } catch (error) {
     console.log(error);
   }
-  
 }
 
-async function doOneCategory(page: puppeteer.Page, link: string) {
+async function doOneCategory(link: string) {
+  const page = await browser.newPage();
   await page.goto(link);
-
-  await page.click(".cookie-alert-extended-button");
 
   const products = await page.evaluate(() => {
     const products = document.querySelectorAll(".ret-o-card");
 
     return Array.from(products).map((product) => {
       const nameElement = product.querySelector("h3.ret-o-card__headline");
-      const discountElement = product.querySelector(".lidl-m-pricebox__discount-price");
-      const priceElement = product.querySelector(".lidl-m-pricebox__price");
+      const priceElement = product.querySelector(
+        ".lidl-m-pricebox__discount-price"
+      );
+      const discountElement = product.querySelector(".lidl-m-pricebox__price");
       //   const discountPercantage = product.querySelector(
       //     ".lidl-m-pricebox__highlight"
       //   );
-      const quantityElement = product.querySelector(".lidl-m-pricebox__basic-quantity");
+      const quantityElement = product.querySelector(
+        ".lidl-m-pricebox__basic-quantity"
+      );
       const image = product
         // .querySelector(".nuc-m-picture__image")
         .querySelector(".nuc-a-source")
         ?.getAttribute("srcset"); // optional chaining here
 
       const name = nameElement?.textContent?.trim();
-      const price = Number.parseFloat(priceElement?.textContent?.trim().replace(",",".") ?? "");
-      const discountedPrice = Number.parseFloat(discountElement?.textContent?.trim().replace(",",".") ?? "");
+      let price = Number.parseFloat(
+        priceElement?.textContent?.trim().replace(",", ".") ?? ""
+      );
+      let discountedPrice: number | null = Number.parseFloat(
+        discountElement?.textContent?.trim().replace(",", ".") ?? ""
+      );
       const unitOfMeasure = quantityElement?.textContent?.trim();
       const storeName = "Lidl";
+
+      if (price === null && discountedPrice !== null) {
+        const temp = price;
+        price = discountedPrice;
+        discountedPrice = temp;
+      }
 
       return {
         name,

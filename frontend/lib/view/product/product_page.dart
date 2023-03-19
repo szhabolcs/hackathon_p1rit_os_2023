1046d37f9dcc2952b12ea3c13fa453417367f5af
RobiA17 <55663657+RobiA17@users.noreply.github.com>
2023-03-19 02:06:49 +0200

    added product page and product card and store card

--- frontend/lib/view/product/product_page.dart
+++ frontend/lib/view/product/product_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/services/product_service.dart';
+import 'package:frontend/widgets/store_card.dart';
 
 class ProductPage extends StatefulWidget {
   const ProductPage({Key? key}) : super(key: key);
@@ -10,6 +12,41 @@ class ProductPage extends StatefulWidget {
 class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                const SizedBox(height: 60,),
+                const Text(
+                  "Our offers",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(22, 66, 118, 1)
+                  ),
+                ),
+                const SizedBox(height: 30,),
+                ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: ProductService().getStoresCount(),
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return StoreCard(
+                      storeName: ProductService().getStoreNameAtIndex(index),
+                      products: ProductService().getProductsByKey(ProductService().getStoreNameAtIndex(index)),
+                    );
+                  }
+                )
+            ],
+            ),
+          ),
+        )
+      ),
+    );
   }
 }

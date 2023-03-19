a398c640a6a5f650f51d96a28d1c131e93b691fa
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-03-19 05:31:06 +0200

    working on list/save

--- backend/src/services/ListService.ts
+++ backend/src/services/ListService.ts
@@ -2,63 +2,91 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function query(list: string[]) {
-    const _list: {
-        Lidl: ProductWithOthers[];
-        Kaufland: ProductWithOthers[];
-    } = {
-        "Lidl": [],
-        "Kaufland": []
-    };
+  const _list: {
+    Lidl: ProductWithOthers[];
+    Kaufland: ProductWithOthers[];
+  } = {
+    Lidl: [],
+    Kaufland: [],
+  };
 
-    _list.Lidl = await getListFromStore(list, "Lidl");
-    _list.Kaufland = await getListFromStore(list, "Kaufland");
+  _list.Lidl = await getListFromStore(list, "Lidl");
+  _list.Kaufland = await getListFromStore(list, "Kaufland");
 
-    return _list;
+  return _list;
 }
 
 async function getListFromStore(list: string[], store: "Lidl" | "Kaufland") {
-    const _list: ProductWithOthers[] = [];
+  const _list: ProductWithOthers[] = [];
 
-    for (const query of list) {
-        const result = await prisma.product.findFirst({
-            where: {
-                AND: [
-                    {name: { search: query }},
-                    {storeName: store}
-                ]
-            },
-        });
-        if (result === null)
-            continue;
+  for (const query of list) {
+    const result = await prisma.product.findFirst({
+      where: {
+        AND: [{ name: { search: query } }, { storeName: store }],
+      },
+    });
+    if (result === null) continue;
 
-        const others = await prisma.product.findMany({
-            where: {
-                AND: [
-                    {name: { search: query }},
-                    {storeName: store},
-                    {id: {not: result.id}},
-                    {image: {not: result.image}}
-                ]
-            },
-        });
+    const others = await prisma.product.findMany({
+      where: {
+        AND: [
+          { name: { search: query } },
+          { storeName: store },
+          { id: { not: result.id } },
+          { image: { not: result.image } },
+        ],
+      },
+    });
 
-        const line = {
-            id: result.id,
-            name: result.name,
-            price: result.price,
-            discountedPrice: result.discountedPrice,
-            unitOfMeasure: result.unitOfMeasure,
-            image: result.image,
-            storeName: result.storeName,
-            others
-        };
+    const line = {
+      id: result.id,
+      name: result.name,
+      price: result.price,
+      discountedPrice: result.discountedPrice,
+      unitOfMeasure: result.unitOfMeasure,
+      image: result.image,
+      storeName: result.storeName,
+      others,
+    };
 
-        result && _list.push(line);
-    }
+    result && _list.push(line);
+  }
 
-    return _list;
+  return _list;
+}
+
+async function save(
+  userId: number,
+  products: string[],
+  query: string,
+  sum: number,
+  storeName: string
+) {
+  const groceryList = await prisma.groceryList.create({
+    data: {
+      userId,
+      storeName,
+      sum,
+      query,
+    },
+  });
+
+  // console.log(groceryList.id);
+  let groceryListLines: any = [];
+  products.forEach((product) => {
+    groceryListLines.push({
+      productId: Number(product),
+      quantity: 1,
+      groceryListId: groceryList.id,
+    });
+  });
+
+  const groceryListLine = await prisma.groceryListLine.createMany({
+    data: groceryListLines,
+  });
 }
 
 export default {
-    query
-}
+  query,
+  save,
+};
